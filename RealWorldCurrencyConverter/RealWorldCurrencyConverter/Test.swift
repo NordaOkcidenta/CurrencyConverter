7c3d285f9e47f2a48bd1848e6ced0015f4fbed30
NordaOkcidenta <145087801+NordaOkcidenta@users.noreply.github.com>
2024-02-11 20:11:28 -0500

    Test1

---
+++ RealWorldCurrencyConverter/RealWorldCurrencyConverter/Test.swift
@@ -0,0 +1,18 @@
+//
+//  Test.swift
+//  RealWorldCurrencyConverter
+//
+//  Created by SamLi on 2024/2/11.
+//
+
+import SwiftUI
+
+struct Test: View {
+    var body: some View {
+        Text("World")
+    }
+}
+
+#Preview {
+    Test()
+}
