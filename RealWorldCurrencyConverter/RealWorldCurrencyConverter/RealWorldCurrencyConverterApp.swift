d216148a65acff9f3b0a50c6cb8444228639d178
NordaOkcidenta <145087801+NordaOkcidenta@users.noreply.github.com>
2024-02-11 19:47:58 -0500

    initialization

---
+++ RealWorldCurrencyConverter/RealWorldCurrencyConverter/RealWorldCurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealWorldCurrencyConverterApp.swift
+//  RealWorldCurrencyConverter
+//
+//  Created by SamLi on 2024/2/11.
+//
+
+import SwiftUI
+
+@main
+struct RealWorldCurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
