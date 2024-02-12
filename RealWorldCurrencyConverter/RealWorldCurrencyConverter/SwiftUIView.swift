7b5841614534357e85cd63faf93909001daf33aa
Johnathan <tangjiaoshou55@gmail.com>
2024-02-11 20:07:47 -0500

    Johnathan ver.1

---
+++ RealWorldCurrencyConverter/RealWorldCurrencyConverter/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  RealWorldCurrencyConverter
+//
+//  Created by Johnathan Tang on 2/11/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text("Hello")
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
